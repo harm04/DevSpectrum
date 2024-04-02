1947a50799847530020a23cc4c077768f5f0cb50
harm04 <har04mali@gmailcom>
2024-04-02 22:09:10 +0530

    changed logo , changed size of post card

--- lib/pages/add_post.dart
+++ lib/pages/add_post.dart
@@ -1,20 +1,13 @@
 import 'dart:io';
-
-import 'package:devspectrum/pages/project_details/feed.dart';
-
 import 'package:devspectrum/providers/user_provider.dart';
 import 'package:devspectrum/resources/firestore_methods.dart';
-import 'package:devspectrum/resources/storage_methods.dart';
 import 'package:devspectrum/responsive/mobileScreen.dart';
-
 import 'package:devspectrum/utils/pickimage.dart';
 import 'package:devspectrum/utils/snackbar.dart';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
-
 import '../models/user.dart';
 
 class AddPost extends StatefulWidget {
@@ -28,16 +21,16 @@ class _AddPostState extends State<AddPost> {
   bool isLoading = false;
   TextEditingController captioncontroller = TextEditingController();
   TextEditingController githubcontroller = TextEditingController();
-    TextEditingController projectcontroller = TextEditingController();
+  TextEditingController projectcontroller = TextEditingController();
   @override
   void dispose() {
     super.dispose();
     captioncontroller.dispose();
     githubcontroller.dispose();
-     projectcontroller.dispose();
+    projectcontroller.dispose();
   }
 
-  // late CollectionReference imgRef;
+
   Uint8List? _image;
   void selectFromGallery() async {
     Uint8List im = await pickImage(ImageSource.gallery);
@@ -81,8 +74,15 @@ class _AddPostState extends State<AddPost> {
       setState(() {
         isLoading = true;
       });
-      res = await FirestoreMethods().uploadPost(_image!, uid, username, profImg,
-          captioncontroller.text, githubcontroller.text,projectcontroller.text,selectedScreenshots);
+      res = await FirestoreMethods().uploadPost(
+          _image!,
+          uid,
+          username,
+          profImg,
+          captioncontroller.text,
+          githubcontroller.text,
+          projectcontroller.text,
+          selectedScreenshots);
       // await StorageMethods().uploadMultipleImages(selectedScreenshots);
       if (res == 'success') {
         setState(() {
@@ -90,12 +90,12 @@ class _AddPostState extends State<AddPost> {
         });
         clearImage();
         // ignore: use_build_context_synchronously
-       await showSnackbar('post uploaded', context);
+        await showSnackbar('post uploaded', context);
         // ignore: use_build_context_synchronously
         Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const mobileScreenLayout()));
+            context,
+            MaterialPageRoute(
+                builder: (context) => const mobileScreenLayout()));
       }
     } catch (err) {
       setState(() {
@@ -108,195 +108,195 @@ class _AddPostState extends State<AddPost> {
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context).getUser;
-    return  isLoading
-                      ? const Center(
-                        child: CircularProgressIndicator(
-                           
+    return isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Post'),
+                  actions: [
+                    _image != null
+                        ? TextButton(
+                            onPressed: () async {
+                              await postProject(
+                                  user.uid, user.username, user.photoUrl);
+                              // ignore: use_build_context_synchronously
+                            },
+                            child: const Text('Upload'))
+                        : const SizedBox()
+                  ],
+                ),
+                body: _image == null
+                    ? Column(
+                        children: [
+                          const SizedBox(
+                            height: 20,
                           ),
+                          const Text(
+                            'Choose photo from',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Card(
+                                color: const Color.fromARGB(255, 14, 87, 184),
+                                elevation: 20,
+                                child: InkWell(
+                                  onTap: () => selectFromGallery(),
+                                  child: Container(
+                                    width: 150,
+                                    height: 60,
+                                    child: const Center(
+                                        child: Text(
+                                      'Gallery',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Card(
+                                color: const Color.fromARGB(255, 14, 87, 184),
+                                elevation: 20,
+                                child: InkWell(
+                                  onTap: () => selectFromCamera(),
+                                  child: Container(
+                                    width: 150,
+                                    height: 60,
+                                    child: const Center(
+                                        child: Text(
+                                      'Camera',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       )
-                      : GestureDetector(
-       onTap: ()=>FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        appBar: AppBar(
-            
-          title: const Text('Post'),
-          actions: [
-            _image != null
-                ? TextButton(
-                    onPressed: () async {
-                      await postProject(user.uid, user.username, user.photoUrl);
-                      // ignore: use_build_context_synchronously
-                      
-                    },
-                    child: const Text('Upload'))
-                : const SizedBox()
-          ],
-        ),
-        body: _image == null
-            ? Column(
-                children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Text(
-                    'Choose photo from',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Card(
-                        color: const Color.fromARGB(255, 14, 87, 184),
-                        elevation: 20,
-                        child: InkWell(
-                          onTap: () => selectFromGallery(),
-                          child: Container(
-                            width: 150,
-                            height: 60,
-                            child: const Center(
-                                child: Text(
-                              'Gallery',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.bold),
-                            )),
+                    : Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(),
+                          const SizedBox(
+                            height: 20,
                           ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Card(
-                        color: const Color.fromARGB(255, 14, 87, 184),
-                        elevation: 20,
-                        child: InkWell(
-                          onTap: () => selectFromCamera(),
-                          child: Container(
-                            width: 150,
-                            height: 60,
-                            child: const Center(
-                                child: Text(
-                              'Camera',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.bold),
-                            )),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              CircleAvatar(
+                                radius: 25,
+                                backgroundImage: NetworkImage(user.photoUrl),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                  child: TextField(
+                                controller: captioncontroller,
+                                decoration: const InputDecoration(
+                                    hintText: 'Write a caption'),
+                                maxLines: null,
+                              )),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                height: 50,
+                                width: 50,
+                                color: Colors.white,
+                                child: Image.memory(
+                                  _image!,
+                                  fit: BoxFit.cover,
+                                ),
+                              )
+                            ],
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                 Container(),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      CircleAvatar(
-                        radius: 25,
-                        backgroundImage: NetworkImage(user.photoUrl),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Expanded(
-                          child: TextField(
-                        controller: captioncontroller,
-                        decoration:
-                            const InputDecoration(hintText: 'Write a caption'),
-                        maxLines: null,
-                      )),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Container(
-                        height: 50,
-                        width: 50,
-                        color: Colors.white,
-                        child: Image.memory(
-                          _image!,
-                          fit: BoxFit.cover,
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'add your project name here.',
-                    style: TextStyle(color: Colors.grey, fontSize: 12),
-                  ),
-                  TextField(
-                    controller: projectcontroller,
-                    decoration: const InputDecoration(
-                        hintText: 'Devspectrum',
-                        hintStyle: TextStyle(fontSize: 14)),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const Text(
-                    'add your github repository here.',
-                    style: TextStyle(color: Colors.grey, fontSize: 12),
-                  ),
-                  TextField(
-                    controller: githubcontroller,
-                    decoration: const InputDecoration(
-                        hintText: 'eg:https://github.com/jhon/devspectrum',
-                        hintStyle: TextStyle(fontSize: 14)),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        pickScreenshots();
-                      },
-                      child: const Text('Select screenshots')),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Expanded(
-                      child: SizedBox(
-                    width: double.infinity,
-                    child: selectedScreenshots.isEmpty
-                        ? const Center(
-                            child: Text('Please select screenshots'),
-                          )
-                        : GridView.builder(
-                            itemCount: selectedScreenshots.length,
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 3,
-                                    crossAxisSpacing: 5,
-                                    mainAxisSpacing: 1.5,
-                                    childAspectRatio: 1),
-                            itemBuilder: (BuildContext context, int index) {
-                              return Center(
-                                  child: kIsWeb
-                                      ? Image.network(
-                                          selectedScreenshots[index].path,
-                                          fit: BoxFit.cover,
-                                        )
-                                      : Image.file(
-                                          selectedScreenshots[index],
-                                          fit: BoxFit.cover,
-                                        ));
-                            }),
-                  ))
-                ],
-              )));
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'add your project name here.',
+                            style: TextStyle(color: Colors.grey, fontSize: 12),
+                          ),
+                          TextField(
+                            controller: projectcontroller,
+                            decoration: const InputDecoration(
+                                hintText: 'Devspectrum',
+                                hintStyle: TextStyle(fontSize: 14)),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            'add your github repository here.',
+                            style: TextStyle(color: Colors.grey, fontSize: 12),
+                          ),
+                          TextField(
+                            controller: githubcontroller,
+                            decoration: const InputDecoration(
+                                hintText:
+                                    'eg:https://github.com/jhon/devspectrum',
+                                hintStyle: TextStyle(fontSize: 14)),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                pickScreenshots();
+                              },
+                              child: const Text('Select screenshots')),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Expanded(
+                              child: SizedBox(
+                            width: double.infinity,
+                            child: selectedScreenshots.isEmpty
+                                ? const Center(
+                                    child: Text('Please select screenshots'),
+                                  )
+                                : GridView.builder(
+                                    itemCount: selectedScreenshots.length,
+                                    gridDelegate:
+                                        const SliverGridDelegateWithFixedCrossAxisCount(
+                                            crossAxisCount: 3,
+                                            crossAxisSpacing: 5,
+                                            mainAxisSpacing: 1.5,
+                                            childAspectRatio: 1),
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      return Center(
+                                          child: kIsWeb
+                                              ? Image.network(
+                                                  selectedScreenshots[index]
+                                                      .path,
+                                                  fit: BoxFit.cover,
+                                                )
+                                              : Image.file(
+                                                  selectedScreenshots[index],
+                                                  fit: BoxFit.cover,
+                                                ));
+                                    }),
+                          ))
+                        ],
+                      )));
   }
 }
--- lib/pages/project_details/feed.dart
+++ lib/pages/project_details/feed.dart
@@ -21,16 +21,35 @@ class _FeedScreenState extends State<FeedScreen> {
       appBar:AppBar(
               backgroundColor: Colors.black,
               centerTitle: false,
-              title: Image.asset('assets/logo/D.png'),
-              actions: [
-                IconButton(
-                  icon: const Icon(
-                    Icons.messenger_outline,
-                    color: Colors.white,
+              title: ShaderMask(
+                shaderCallback: (Rect bounds) {
+                  const gradient = LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Color.fromARGB(255, 79, 0, 143), Color.fromARGB(255, 247, 108, 228),Color.fromARGB(255, 11, 94, 218),Color.fromARGB(255, 54, 73, 244)],
+                  );
+                            
+                  // using bounds directly doesn't work because the shader origin is translated already
+                  // so create a new rect with the same size at origin
+                  return gradient.createShader(Offset.zero & bounds.size);
+                },
+                child: const Text(
+                  'DevSpectrum',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.white, // must be white for the gradient shader to work
                   ),
-                  onPressed: () {},
                 ),
-              ],
+              ),
+              // actions: [
+              //   IconButton(
+              //     icon: const Icon(
+              //       Icons.messenger_outline,
+              //       color: Colors.white,
+              //     ),
+              //     onPressed: () {},
+              //   ),
+              // ],
             ),
       body:
        StreamBuilder(
